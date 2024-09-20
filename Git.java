82c9c3a714547dfdae97f4a841cff2eccf37ec0c
Roen <rbeiley1@hwemail.com>
2024-09-20 10:44:11 -0700

    Day 1
    Made initGitRepo()- still need to test it out and then do milestone 2 stuff

--- Git.java
+++ Git.java
@@ -1,3 +1,30 @@
+import java.io.File;
+import java.io.IOException;
+
 public class Git {
-    
+    public static void main(String[] args) throws IOException {
+        initGitRepo();
+    }
+
+    public static void initGitRepo() throws IOException {
+        int existsCounter = 0;
+        File gitFile = new File("git");
+        gitFile.mkdir();
+        if (gitFile.exists()) {
+            existsCounter++;
+        }
+        File objectsFile = new File("git/objects");
+        objectsFile.mkdir();
+        if (objectsFile.exists()) {
+            existsCounter++;
+        }
+        File index = new File("git/objects/index");
+        if (index.exists()) {
+            existsCounter++;
+        }
+        index.createNewFile();
+        if (existsCounter >= 3) {
+            System.out.println("Git Repository already exists");
+        }
+    }
 }
